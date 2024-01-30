116a4a5d0645b74bc8033c3ad764e10bfeae00d7
Seiji Ota <ota.seiji@jvckenwood.com>
2024-01-30 15:23:44 +0900

    Buffered Play

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const ctx = new AudioContext();
 
-const audioElement = document.querySelector("audio");
-// Web Audio API内で使える形に変換
-const track = ctx.createMediaElementSource(audioElement);
+let sampleSource;
+let gainNode;  // GainNodeを格納する変数
+let isPlaying = false;
 
-document.querySelector("#play").addEventListener("click", () => {
-    if(ctx.state === "suspended") {
-        ctx.resume();
-    }
+// 音源を取得しAudioBuffer形式に変換して返す関数
+async function setupSample() {
+    const response = await fetch("./assets/sample.mp3");
+    const arrayBuffer = await response.arrayBuffer();
+    // Web Audio APIで使える形式に変換
+    const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+}
+
+// AudioBufferをctxに接続し再生する関数
+function playSample(ctx, audioBuffer) {
+    sampleSource = ctx.createBufferSource();
+    gainNode = ctx.createGain();  // GainNodeを作成
+
+    // 変換されたバッファーを音源として設定
+    sampleSource.buffer = audioBuffer;
+
+    // GainNodeを接続
+    sampleSource.connect(gainNode);
+    gainNode.connect(ctx.destination);
+
     // 出力につなげる
-    track.connect(ctx.destination);
-    audioElement.play();
+    sampleSource.start();
+    isPlaying = true;
+}
+
+// エフェクトを追加する関数
+function applyEffect() {
+    // 独自のエフェクトをここに追加
+    // 例: 音量を変更する
+    gainNode.gain.value = 1.0;  // 音量を半分に変更
+}
+
+document.querySelector("#play").addEventListener("click", async () => {
+    // 再生中なら二重に再生されないようにする
+    if (isPlaying) return;
+    const sample = await setupSample();
+    playSample(ctx, sample);
+    applyEffect();  // エフェクトを適用
 });
 
-// audioElementを一時停止する
-document.querySelector("#pause").addEventListener("click", () => {
-    audioElement.pause();
+// oscillatorを破棄し再生を停止する
+document.querySelector("#stop").addEventListener("click", async () => {
+    sampleSource?.stop();
+    isPlaying = false;
 });
